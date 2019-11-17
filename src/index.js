240296fd567a9b667027a8a58c00aa8507fbade5
Ogundiji Bolade <kamponistullar@gmail.com>
2019-11-17 09:28:59 +0100

    Added best solutions

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 function queueTime(customers, n) {
   let total = 0;
-  let startpoint = n - 1;
-  let customerQueue =
-    n !== 1
-      ? customers.filter((value, index) => startpoint !== index)
-      : customers;
-  total =
-    customerQueue.length > 0
-      ? customerQueue.reduce((val, num) => {
-          return val + num;
-        })
-      : 0;
+  let till = customers.length > n ? customers.slice(n) : customers.slice(0, n);
+  if (customers.length > 0 && customers.length > n) {
+    let customerQueue =
+      n !== 1
+        ? till.reduce((val, num, index) => {
+            customerQueue[index] += num;
+            return customerQueue;
+          })
+        : customers;
+    total = customerQueue.reduce((val, num) => {
+      return val + num;
+    });
+  } else {
+    total = 0;
+  }
+
   return total;
 }
 
-let test = queueTime([], 3);
+// Best Solution
+/*
+function queueTime(customers, n) {
+  var w = new Array(n).fill(0);
+  for (let t of customers) {
+    let idx = w.indexOf(Math.min(...w));
+    w[idx] += t;
+  }
+  return Math.max(...w);
+}
+*/
+
+let test = queueTime([1, 2], 1);
 console.log(test);
