b5afa72492c53c6d6b9e6763f69f2ba068904ab3
Ogundiji Bolade <kamponistullar@gmail.com>
2019-11-17 08:01:00 +0100

    modified to meet test requirements

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 function queueTime(customers, n) {
   let total = 0;
-  let startpoint = n-1;
-  let customerQueue = (n !== 1)? customers.filter((value, index) => startpoint !== index): customers;
-  total = customerQueue.reduce((val, num) => {
-    return val + num;
-  });
+  let startpoint = n - 1;
+  let customerQueue =
+    n !== 1
+      ? customers.filter((value, index) => startpoint !== index)
+      : customers;
+  total =
+    customerQueue.length > 0
+      ? customerQueue.reduce((val, num) => {
+          return val + num;
+        })
+      : 0;
   return total;
 }
 
-let test = queueTime([5,3,4], 1);
+let test = queueTime([], 3);
 console.log(test);
