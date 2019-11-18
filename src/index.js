d0c28c3db960b607f3b2cae8795940ba142e194d
Ogundiji Bolade <kamponistullar@gmail.com>
2019-11-18 13:18:29 +0100

    Unable to solve spaces

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 function solve(str) {
-  let string = str.split("");
-  return string.reverse().join("");
+  let string = str
+    .split(" ")
+    .join("")
+    .split("");
+  let words = str.split(" ");
+  // Get the length of each words in the array
+  let eachWordLength = [];
+  for (let i = 0; i < words.length; i++) {
+    eachWordLength.push(words[i].length);
+  }
+  // Reverse the input string
+  let reverseString = string.reverse();
+  let newWords = [];
+  for (let i = 0; i < reverseString.length; i++) {
+    newWords += reverseString[i];
+  }
+
+  return newWords;
 }
 
-let test = solve("our code");
+let test = solve("your code rocks");
 console.log(test);
