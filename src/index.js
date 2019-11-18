8f9181fdfaaea51f2486360b64e53a218111ca7f
Ogundiji Bolade <kamponistullar@gmail.com>
2019-11-18 14:38:11 +0100

    Added passed best solution

--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@ function sumOfDifferences(arr) {
     return b - a;
   });
   let sumArray = [];
-  arr.forEach((val, index) => {
-    sumArray += val;
-  });
   for (let i = 0; i < arr.length; i++) {
     for (let j = 1; j < arr.length; j++) {
-      return (sumArray += arr[i] - arr[j] + (arr[j] - 1));
+      return (sumArray = arr[i] - arr[j] + (arr[j] - 1));
     }
   }
 }
 
 let test = sumOfDifferences([2, 1, 10]);
 console.log(test);
+
+/* Best solutions that passed */
+/*
+
+function sumOfDifferences(arr) {
+  arr.length <= 1 ? 0 : Math.max(...arr) - Math.min(...arr);
+ }
+ 
+ */
