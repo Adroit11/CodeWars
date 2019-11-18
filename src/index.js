c5a8217e0d3cc98120a5eb70542b5ad90e110f80
Ogundiji Bolade <kamponistullar@gmail.com>
2019-11-18 09:10:51 +0100

    twoSum challenge
    Solution to the twoSum challenge

--- src/index.js
+++ src/index.js
@@ -1,35 +1,12 @@
-function queueTime(customers, n) {
-  let total = 0;
-  let till = customers.length > n ? customers.slice(n) : customers.slice(0, n);
-  if (customers.length > 0 && customers.length > n) {
-    let customerQueue =
-      n !== 1
-        ? till.reduce((val, num, index) => {
-            customerQueue[index] += num;
-            return customerQueue;
-          })
-        : customers;
-    total = customerQueue.reduce((val, num) => {
-      return val + num;
-    });
-  } else {
-    total = 0;
+function twoSum(numbers, target) {
+  let indexes = {};
+  numbers.forEach((val, index) => (indexes[val] = index));
+  for (let i = 0; i < numbers.length; i++) {
+    if (indexes[target - numbers[i]]) {
+      return [indexes[target - numbers[i]], i];
+    }
   }
-
-  return total;
 }
 
-// Best Solution
-/*
-function queueTime(customers, n) {
-  var w = new Array(n).fill(0);
-  for (let t of customers) {
-    let idx = w.indexOf(Math.min(...w));
-    w[idx] += t;
-  }
-  return Math.max(...w);
-}
-*/
-
-let test = queueTime([1, 2], 1);
+let test = twoSum([1, 2, 3], 5);
 console.log(test);
---
+++ src/twoSum.js
@@ -0,0 +1,12 @@
+function twoSum(numbers, target) {
+  let indexes = {};
+  numbers.forEach((val, index) => (indexes[val] = index));
+  for (let i = 0; i < numbers.length; i++) {
+    if (indexes[target - numbers[i]]) {
+      return [indexes[target - numbers[i]], i];
+    }
+  }
+}
+
+let test = twoSum([1, 2, 3], 5);
+console.log(test);
